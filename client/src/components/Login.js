9fc004bd583835c9a8072bad213fe46c99067b58
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-09 12:48:25 -0500

    lecture work-in-progress

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -21,6 +21,8 @@ class Login extends React.Component {
   login = e => {
     e.preventDefault();
     //1. axios call http://localhost:5000/api/login pass in username and password through the body
+    console.log(this.state.credentials);
+    
     axios.post('http://localhost:5000/api/login', this.state.credentials)
       .then(resp=> {
         //2. if the call is successful: save token in localStorage

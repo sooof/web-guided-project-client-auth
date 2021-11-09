368cd6100554d04d1871b3797a5419cf4ab1e33d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-09 13:46:17 -0500

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component:Component, ...rest}) => {
-    return <Route {...rest} component={Component}/>
+    return <Route {...rest} render={()=> {
+        return <h2>THIS IS THE PROTECTED ROUTE!!!!!</h2>
+    }}/> //component={Component}/>
 }
 
 export default PrivateRoute;

87b839261d55a7d97b333dbef85eaf8d5d604fff
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-09 13:00:01 -0500

    lecture work-in-progress

--- client/src/components/GasPrices.js
+++ client/src/components/GasPrices.js
@@ -9,7 +9,11 @@ class GasPrices extends React.Component {
   };
   
   componentDidMount() {
-    axios.get('http://localhost:5000/api/data')
+    axios.get('http://localhost:5000/api/data', {
+      headers: {
+        authorization: localStorage.getItem('token')
+      }
+    })
       .then(resp=> {
         console.log(resp);
       })

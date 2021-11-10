da10f1bf03ecab162d72016e75d5204c23f91d1c
sooof <sooof@me.com>
2021-11-10 13:59:47 -0800

    in PrivateRoute can see prop, but  other  can't see

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component:Component, ...rest}) => {
-    return <Route {...rest}   render={()=>{
+    return <Route {...rest}   render={(props)=>{
+        console.log("PrivateRoute ", props)
         if(localStorage.getItem('token')){
             return <Component />
         }else {

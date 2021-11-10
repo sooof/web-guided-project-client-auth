60ebe3c819425301babf18458bbfb3c9b07be171
sooof <sooof@me.com>
2021-11-09 19:22:33 -0800

    Logout.js usdHistory

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React,{useEffect}  from "react";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Logout = ()=> {
+    console.log("Logout  history = ", useHistory());
+    useEffect(() =>{
+    const token = localStorage.getItem("token")
+        axios.post('http://localhost:3000/api/logout', {
+        headers: {
+            authorization: token
+            }
+        })
+        .then(resp => {
+            //console.log("Logout  resp = ", resp);
+            localStorage.removeItem('token')
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
     return(<div></div>);
 }
 

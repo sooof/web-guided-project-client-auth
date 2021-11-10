62a53df32793a9575fad3b2bf42147f1eb5b776a
sooof <sooof@me.com>
2021-11-09 19:32:20 -0800

    Logout.js usdHistory to push /login

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -2,8 +2,11 @@ import React,{useEffect}  from "react";
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
-const Logout = ()=> {
+const Logout = (props)=> {
+    console.log("Logout  props = ", props);
+    console.log("Logout  props.history = ", props.history );
     console.log("Logout  history = ", useHistory());
+    const {push} = useHistory()
     useEffect(() =>{
     const token = localStorage.getItem("token")
         axios.post('http://localhost:3000/api/logout', {
@@ -14,6 +17,7 @@ const Logout = ()=> {
         .then(resp => {
             //console.log("Logout  resp = ", resp);
             localStorage.removeItem('token')
+            push('./login')
         })
         .catch(err => {
             console.log(err);

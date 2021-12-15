b4de6698678f8f4346690c839e2726a91baf6b6e
sooof <sooof@me.com>
2021-12-14 18:45:50 -0800

    App.js: Use PrivateRoute pass data, show <p>..</p>

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = (component, ...rest) => {
+const PrivateRoute = ({component, ...rest}) => {
     console.log(component);
     console.log(rest);
-    return <Route {...rest}/>
+    return <Route {...rest} render={()=> {
+        return(<p>THIS IS A PROTECTED ROUTE</p>);
+    }}/>
 }
 
 export default PrivateRoute;

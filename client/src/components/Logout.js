0d5d02b25bc639b4622b6052fce2660d94f9c502
sooof <sooof@me.com>
2021-12-14 16:46:11 -0800

    Logout: axios.post work localStorage.removeItem('token')

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 
 const Logout = ()=> {
+
+    useEffect(()=> {
+        // const token = localStorage.getItem("token");
+        axios.post('http://localhost:5000/api/logout', {}, {
+            headers:{
+                authorization: localStorage.getItem('token')
+            }
+        }).then(resp => {
+                localStorage.removeItem('token');
+                // push('/login');
+            });
+    }, []);
     return(<div></div>);
 }
 

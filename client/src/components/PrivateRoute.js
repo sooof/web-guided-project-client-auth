34705cb2be56b531aba8f9712bb16d887cfc9683
sooof <sooof@me.com>
2021-12-14 18:22:57 -0800

    App.js: Use PrivateRoute After Login, still can see page http://localhost:1234/protected

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = () => {
+const PrivateRoute = (props) => {
     return <Route {...props}/>
 }
 

8abb6cb12bf03086bfcad3b72921e29f27108b24
sooof <sooof@me.com>
2021-12-14 18:43:50 -0800

    App.js: Use PrivateRoute pass  (component, ...rest) After Login,  can't see page /protected

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = (props) => {
-    return <Route {...props}/>
+const PrivateRoute = (component, ...rest) => {
+    console.log(component);
+    console.log(rest);
+    return <Route {...rest}/>
 }
 
 export default PrivateRoute;
